f3717862d5b52ac8195320627fb4a5e7a7c7c22f
pulchroxloom <dantevbarbieri@gmail.com>
2018-11-16 10:28:40 -0600

    Add basic ui and prototype features
    Visuals set out and some simple functionality present.

---
+++ app/src/main/java/com/example/dante/tammodertates/Home.kt
@@ -0,0 +1,36 @@
+package com.example.dante.tammodertates
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+
+class Home : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun toastApp(view: View) {
+        val tst = Toast.makeText(this, "That not supported yet!", Toast.LENGTH_SHORT)
+        tst.show()
+    }
+
+    fun openSignIn(view: View) {
+        val url = "https://goo.gl/forms/z9J9CWtFwHciRRGl2"
+
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    fun openSocialMediaActivity(view: View) {
+        val social_intent = Intent(this, Social_Media::class.java)
+        startActivity(social_intent)
+    }
+}
---
+++ app/src/main/java/com/example/dante/tammodertates/Social_Media.kt
@@ -0,0 +1,12 @@
+package com.example.dante.tammodertates
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Social_Media : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social_media)
+    }
+}
