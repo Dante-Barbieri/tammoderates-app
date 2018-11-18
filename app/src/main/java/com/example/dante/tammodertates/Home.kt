8539619459ff6a9b16d732fc626de8067483eb5f
pulchroxloom <dantevbarbieri@gmail.com>
2018-11-18 10:46:00 -0600

    Add rotation constraints and support buttons on social media activity

--- app/src/main/java/com/example/dante/tammodertates/Home.kt
+++ app/src/main/java/com/example/dante/tammodertates/Home.kt
@@ -2,6 +2,8 @@ package com.example.dante.tammodertates
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
@@ -14,6 +16,7 @@ class Home : Activity() {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_home)
+        requestedOrientation = SCREEN_ORIENTATION_SENSOR_PORTRAIT
     }
 
     fun toastApp(view: View) {
--- app/src/main/java/com/example/dante/tammodertates/Social_Media.kt
+++ app/src/main/java/com/example/dante/tammodertates/Social_Media.kt
@@ -1,12 +1,47 @@
 package com.example.dante.tammodertates
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Social_Media : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social_media)
+    }
+
+    fun openTwitter(view: View) {
+        val url = "https://twitter.com/tammoderates?lang=en"
+
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    fun openInstagram(view: View) {
+        val url = "http://instagram.com/_u/tammoderates"
+
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    fun openFacebook(view: View) {
+        val url = "https://m.facebook.com/tammoderates/"
+
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    fun openWebsite(view: View) {
+        val url = "https://tammoderates.wixsite.com/tamm"
+
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
